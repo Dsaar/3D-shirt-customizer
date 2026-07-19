aa043f7eb17d8695ed945674755a9d60d6aa1af4
Dsaar <danieljoshuasaar@gmail.com>
2026-07-20 00:13:00 +0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import imageRoutes from './routes/image.routes.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(
+	cors({
+		origin: (origin, callback) => {
+			console.log("Request origin:", origin);
+			callback(null, true);
+		},
+	})
+);
+
+app.use(express.json({ limit: "50mb" }))
+
+app.use("/api/v1/images", imageRoutes);
+
+
+app.get('/', (req, res) => {
+	res.status(200).json({ message: "Hello from DALL.E" })
+})
+
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
+});
